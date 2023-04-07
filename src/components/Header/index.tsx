e8567771121a4e2180ba4abbe796ef072af6ec5e
Rafael Rabelo <rafaelrabelo1610@gmail.com>
2023-04-06 22:10:54 -0300

    add image.png in project react typescript vite

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,20 @@
-import { Container } from "./styles";
+import { Container, Image } from "./styles";
+import logo from "./../../assets/img/logomarca.png";
 
 export function Header() {
   return (
     <Container>
-      Header
+      <div>
+        <Image src={logo} />
+      </div>
+
+      <div>
+        links
+      </div>
+
+      <div>
+        icons
+      </div>
     </Container>
   )
 }
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,7 +1,19 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  background-color: ${props => props.theme.COLORS.RED_500};
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  background-color: ${props => props.theme.COLORS.WHITE};
+  
+  padding: 20px 40px;
   height: 100px;
   width: 100%;
+`;
+
+export const Image = styled.img`
+  width: 60px;
+  height: 60px;
+  object-fit: cover;
 `;
